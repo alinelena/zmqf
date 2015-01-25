e154ae6fa893c8fc9e3a5add72c9c193748909a3
Alin Marin Elena <alinm.elena@gmail.com>
2015-01-25 01:15:58 +0000

    version example

--- zmqhello.F90
+++ zmqhello.F90
@@ -4,7 +4,7 @@ program zmqhello
 
   integer :: major,minor,patch
 
-  call zmqVersion(major,minor,patch)
+  call zmq_version(major,minor,patch)
 
   write(*,'(a,i0,".",i0,".",i0)')"zeromq version ",major,minor,patch
 
