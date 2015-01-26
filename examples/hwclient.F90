f0c0a0fbdc235f6b4a4ce6ae1b493ee80f76977d
Alin Marin Elena <alinm.elena@gmail.com>
2015-01-26 08:13:42 +0000

    updated a little bit the example

--- examples/hwclient.F90
+++ examples/hwclient.F90
@@ -5,9 +5,11 @@ program hwclient
 
   type(c_ptr) :: context
   type(c_ptr) :: requester
+  type(c_ptr) :: err
   integer     :: rc 
   integer     :: ierror
   character, target   :: buffer(10)
+  character(len=200),pointer :: errmsg
   integer(c_size_t) :: lb=10
 
   context = zmq_ctx_new()
@@ -15,6 +17,10 @@ program hwclient
   rc = zmq_connect (requester, "tcp://*:5555"//C_NULL_CHAR)
 
   if (rc /= 0) then
+!   allocate(errmsg(200))
+    err = zmq_strerror_c(rc)
+    call c_f_pointer(err,errmsg)
+    write(*,'(a,i0)'),errmsg,len(trim(errmsg))
     write(*,'(a,i0)')"Failure to connect with code: ",rc
     stop -1
   end if
